6c3254344f8bdd86e0648bf280984883781a44d2
Priyanka Jain <stunningpriyanka19@gmail.com>
2015-05-27 14:53:32 +0530

    Created new module called trytond product bin location #7870

--- setup.py
+++ setup.py
@@ -82,7 +82,9 @@ minor_version = int(minor_version)
 
 requires = []
 
-MODULE2PREFIX = {}
+MODULE2PREFIX = {
+    'product_notebook': 'openlabs',
+}
 
 MODULE = "product_storage_area"
 PREFIX = "openlabs"
